6b981a7c7faee2ce60c7d43b3a2d5840aee866c2
maqinglong <maqinglong1999@gmail.com>
2017-01-04 22:53:09 +0800

    待合体

---
+++ app/src/main/java/maxiaobu/mqltemplate/activity/patterns/AdDialogActivity.java
@@ -0,0 +1,32 @@
+package maxiaobu.mqltemplate.activity.patterns;
+
+import android.os.Bundle;
+
+import maxiaobu.mqltemplate.R;
+import maxiaobu.mqltemplate.base.BaseAty;
+/**
+    *Created by 马小布 on 2017/1/4.
+    *introduction: 真他娘的不知道说点啥
+    *email:maxiaobu1999@163.com
+    *功能：测试广告活动弹窗demo
+    *伪码：
+    *待完成：
+*/
+public class AdDialogActivity extends BaseAty {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_dialog);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
--- app/src/main/java/maxiaobu/mqltemplate/fragment/PatternsFragment.java
+++ app/src/main/java/maxiaobu/mqltemplate/fragment/PatternsFragment.java
@@ -1,24 +1,28 @@
 package maxiaobu.mqltemplate.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.OnClick;
 import maxiaobu.mqltemplate.R;
+import maxiaobu.mqltemplate.activity.patterns.BannersActivity;
 import maxiaobu.mqltemplate.base.BaseFrg;
 
 /**
  * Created by 马小布 on 2017/1/3.
  * introduction：我长得真他娘的磕碜，单身未娶，求包养
  * email：maxiaobu1216@gmail.com
- * 功能：android场景模版
+ * 功能：android场景模版列表
  * 伪码：
  * 待完成：
  */
-public class PatternsFragment extends BaseFrg {
+public class PatternsFragment extends BaseFrg implements View.OnClickListener{
 
     private View mRootView;
 
@@ -45,4 +49,16 @@ public class PatternsFragment extends BaseFrg {
 
     }
 
+    @OnClick({R.id.btn_banner})
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_banner:
+                startActivity(new Intent(mActivity, BannersActivity.class));
+                break;
+            default:
+                break;
+        }
+
+    }
 }
